e73442131b856c3313e7953e273b0e9f771e6ec8
sormiston <sean.david.ormiston@gmail.com>
2025-04-11 19:45:34 +0100

    uses vue-define-options macro to keep setup sugar in vue SFCs for component naming

--- packages/components/src/button/button.vue
+++ packages/components/src/button/button.vue
@@ -1,9 +1,6 @@
 <template>
   <button>Test Button 🥲</button>
 </template>
-<script lang="ts">
-import { defineComponent } from "vue";
-export default defineComponent({
-  name: "IMHO-Button", // Developer note: I am necessary for component registration!  Comment me out to see dev error.
-});
+<script lang="ts" setup>
+defineOptions({ name: "IMHO-Button" }); // Developer note: I am necessary for component registration!  Comment me out to see dev error.
 </script>
